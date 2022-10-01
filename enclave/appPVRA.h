ad360a65b32df5553dc1782028002700cd37731b
Mohammad Behnia <behmohmay@gmail.com>
2022-10-01 21:43:39 +0000

    keep appPVRA files in enclave, change to empty application

---
+++ enclave/appPVRA.h
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+
+#define INITSEALSIZE 7092
+
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#define NUM_COMMANDS 2
+#define NUM_USERS 2
+#define NUM_TESTS 100
+
+struct cInputs
+{
+	int uid;
+	int test_result;
+};
+
+struct cResponse
+{
+	bool access;
+	int error;
+	char message[100];
+};
+
+struct AD
+{
+	char test_history[NUM_USERS*NUM_TESTS];
+	int num_tests[NUM_USERS];
+	int query_counter[NUM_USERS];
+};
+
+#endif
